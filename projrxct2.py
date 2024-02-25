b5b24f73984f45dc5d8f4c500e8f3d893e9b88cb
akshaysagar6 <151427733+akshaysagar6@users.noreply.github.com>
2024-02-25 08:20:25 +0530

    Update projrxct2.py

--- projrxct2.py
+++ projrxct2.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from pickle import load
 import numpy as np
-import xgboost
+pip install xgboost
 
 st.title('Combined Cycle Power Plant')
 st.subheader('Regression Model To Predict The Energy Generated')
